f0e7395e052431d2508d892f19933f979142908e
Anshul Garg <anshul@haveignition.com>
2025-08-28 08:32:14 +0530

    minor change

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,6 @@ const nextConfig = {
     images: {
         unoptimized: true, // needed if using <Image /> with static export
     },
-    async rewrites() {
-        return [
-            {
-                // Match anything under /blog/* EXCEPT /blog or /blog/
-                source: '/blog/:path((?!^$).+)', // This means: match if :path is NOT empty
-                destination: 'https://blog.mydocsy.com/:path',
-            },
-        ];
-    },
 };
 
 export default nextConfig;
